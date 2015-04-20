60d8777088ba070cc32636a3d3b788f7cc994bb5
royzhao <524510356@qq.com>
2015-04-20 18:17:19 +0800

    入参的bug修复

--- bin.js
+++ bin.js
@@ -4,18 +4,17 @@ var minimist = require('minimist')
 var docker = require('./')
 
 var argv = minimist(process.argv, {
-  alias: {port:'p',redis_addr:'r',docker_host:'dh', docker_port:'dp', help:'h'},
+  alias: {port:'p',redis_addr:'r',docker_host:'d', docker_port:'e', help:'h'},
   default: {port:process.env.PORT || 8080}
 })
 
 var redis_addr = argv.redis_addr
-
+console.log(argv)
 if (argv.help ||!argv.docker_host) {
   console.log('Usage: docker-browser-server [options]')
-  console.log()
   console.log('  --port,    -p  [8080]          (port to listen on)')
-  console.log('  --docker_host,  -dh')
-  console.log('  --docker_port,  -dp')
+  console.log('  --docker_host,  -d')
+  console.log('  --docker_port,  -e')
   console.log('  --redis_addr,  -r')
   return process.exit(argv.help ? 0 : 1)
 }
